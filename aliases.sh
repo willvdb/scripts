455c0aaeba0c9932d1ca4180084ab86ac4d6c19a
will <wvandenberghe@grandeurpeakglobal.com>
2022-06-03 10:17:45 -0600

    adding toolkit common alias

--- aliases.sh
+++ aliases.sh
@@ -35,6 +35,7 @@ c(){
 alias api='cd ~/code/toolkit-api'
 alias gui='cd ~/code/toolkit-ui'
 alias func='cd ~/code/toolkit-functions'
+alias common='cd ~/code/toolkit-common'
 alias apidev="api && npm run dev"
 alias guidev="gui && npm run serve"
 killonport(){
