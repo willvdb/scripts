d5d257fa18f748aaca0be05e5d97aca6d4c3bc4a
will <wvandenberghe@grandeurpeakglobal.com>
2022-08-25 10:03:26 -0600

    add dev api to docker scripts

--- docker_scripts.sh
+++ docker_scripts.sh
@@ -25,3 +25,30 @@ buildAndDeployApi() {
   buildApi
   deployApi
 }
+
+buildDevApi() {
+
+  cd ~/code/toolkit-api
+
+  docker build \
+  -t toolkit-api:dev \
+  --build-arg DB_CONNECTION_STRING=$DB_DEV_CONNECTION_STRING \
+  --build-arg ASB_CONNECTION_STRING=$ASB_DEV_CONNECTION_STRING \
+  --build-arg COMPANY_QUEUE=$COMPANY_QUEUE \
+  --build-arg OKTA_CLIENTID=$OKTA_CLIENTID \
+  --build-arg OKTA_ISSUER=$OKTA_ISSUER \
+  --build-arg OKTA_AUDIENCE=$OKTA_AUDIENCE .
+}
+
+#you'll need to be signed into azure via the cli for this to work
+deployDevApi() {
+  docker tag toolkit-api:dev grandeurpeakcontainerregistry.azurecr.io/toolkit-api:dev
+
+  docker push grandeurpeakcontainerregistry.azurecr.io/toolkit-api:dev
+}
+
+
+buildAndDeployDevApi() {
+  buildDevApi
+  deployDevApi
+}
