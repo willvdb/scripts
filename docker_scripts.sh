f67631bf4dff6185087012e477e6a15b4c69a264
will <wvandenberghe@grandeurpeakglobal.com>
2025-02-14 12:08:10 -0700

    changes

--- docker_scripts.sh
+++ docker_scripts.sh
@@ -1,14 +1,19 @@
 
 buildApi() {
 
-  cd ~/code/toolkit-api
+  cd ~/code/old-api
+
+  load ~/scripts/env_prod.sh
 
   docker build \
   -t toolkit-api \
-  --build-arg DB_CONNECTION_STRING=$DB_PROD_CONNECTION_STRING \
-  --build-arg ASB_CONNECTION_STRING=$ASB_PROD_CONNECTION_STRING \
+  --build-arg DB_CONNECTION_STRING=$DB_CONNECTION_STRING \
+  --build-arg ASB_CONNECTION_STRING=$ASB_CONNECTION_STRING \
   --build-arg COMPANY_QUEUE=$COMPANY_QUEUE \
   --build-arg OVERRIDE_QUEUE=$OVERRIDE_QUEUE \
+  --build-arg DATA_LAKE_USERNAME=$DATA_LAKE_USERNAME \
+  --build-arg DATA_LAKE_PASSWORD=$DATA_LAKE_PASSWORD \
+  --build-arg DATA_LAKE_SERVER=$DATA_LAKE_SERVER \
   --build-arg OKTA_CLIENTID=$OKTA_CLIENTID \
   --build-arg OKTA_ISSUER=$OKTA_ISSUER \
   --build-arg OKTA_AUDIENCE=$OKTA_AUDIENCE .
@@ -29,13 +34,19 @@ buildAndDeployApi() {
 
 buildDevApi() {
 
-  cd ~/code/toolkit-api
+  load ~/scripts/env_dev.sh
+
+  cd ~/code/old-api
 
   docker build \
-  -t toolkit-api:dev \
-  --build-arg DB_CONNECTION_STRING=$DB_DEV_CONNECTION_STRING \
-  --build-arg ASB_CONNECTION_STRING=$ASB_DEV_CONNECTION_STRING \
+  -t toolkit-api \
+  --build-arg DB_CONNECTION_STRING=$DB_CONNECTION_STRING \
+  --build-arg ASB_CONNECTION_STRING=$ASB_CONNECTION_STRING \
   --build-arg COMPANY_QUEUE=$COMPANY_QUEUE \
+  --build-arg OVERRIDE_QUEUE=$OVERRIDE_QUEUE \
+  --build-arg DATA_LAKE_USERNAME=$DATA_LAKE_USERNAME \
+  --build-arg DATA_LAKE_PASSWORD=$DATA_LAKE_PASSWORD \
+  --build-arg DATA_LAKE_SERVER=$DATA_LAKE_SERVER \
   --build-arg OKTA_CLIENTID=$OKTA_CLIENTID \
   --build-arg OKTA_ISSUER=$OKTA_ISSUER \
   --build-arg OKTA_AUDIENCE=$OKTA_AUDIENCE .
@@ -43,7 +54,7 @@ buildDevApi() {
 
 #you'll need to be signed into azure via the cli for this to work
 deployDevApi() {
-  docker tag toolkit-api:dev grandeurpeakcontainerregistry.azurecr.io/toolkit-api:dev
+  docker tag toolkit-api:latest grandeurpeakcontainerregistry.azurecr.io/toolkit-api:dev
 
   docker push grandeurpeakcontainerregistry.azurecr.io/toolkit-api:dev
 }
